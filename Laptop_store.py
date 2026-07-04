3624fbd503644224be47366fc8d97d1fb99bdc53
Ansh-CodeForever <ankur.ashok.g@gmail.com>
2026-07-04 10:52:08 +0000

    Modifyed Laptop_store

--- Laptop_store.py
+++ Laptop_store.py
@@ -8,21 +8,33 @@ laptop = dict.fromkeys(specs)
 
 master_dict = {}
 master_dict['Brands'] = ['WDell', 'WLenovo', 'WMac', 'WAcer', 'WAcus', 'WHP', 'Apple']
-master_dict['Models'] = ['WIntel i5', 'WIntel i7', 'Wamd Ryzen']
-master_dict['CPUs'] = ['WDell', 'WLenovo', 'WMac', 'WAcer', 'WAcus', 'WHP']
+master_dict['Models'] = ['Dell XPS 13', 'Apple MacBook Air', 'Lenovo ThinkPad X1 Crbon']
+master_dict['CPUs'] = ['WIntel i5', 'WIntel i7', 'Wamd Ryzen']
 master_dict['Speeds'] = ['900 MHz', '2 GHz', '3 GHz', '', '4.5 GHZ']
 master_dict['RAMs'] = ['1 GB', '2 GB', '2.5 GB', '4 GB', '6.5 GB', '8 GB']
 master_dict['Storages'] = ['128 GB', '226 GB', '546 GB', '750 GB', '867 GB', '1 TB']
 master_dict['Screensizes'] = ['9 in', '12 in', '13 in', '14.9 in', '15.5 in', '17 in']
-master_dict['Prices'] = ['EUR', random.randint(500, 800)]
+master_dict['Prices'] = [' EUR 550', 'EUR 600', 'EUR 700', 'EUR 750', 'EUR 800']
+
+models = dict.fromkeys(master_dict['Brands'])
+models['WDell'] = ['Inspire', 'Altitude', 'Adam']
+models['WLenovo'] = ['Think', 'Idea', 'Yoga']
+models['WMac'] = ['MacBook Retnia', 'MacBook Air', 'MacBook Pro']
+models['Wacer'] = ['Lite', 'Aspire', 'Nitro']
+models['WAcus'] = ['Vivo', 'TUF', 'Zen']
+models['WHP'] = ['Mini', 'Maxima', 'Pro']
+models['Apple'] = ['MacBook Air', 'MacBook Pro', 'MackBook Neo']
+
 
 laptop_list = []
 n_laptops = 50
 
-for _ in range(n_laptops):
+for n_laptops in range(50):
     new_laptop = dict.fromkeys(specs)
     for kk in new_laptop:
-        new_laptop[kk] = random.choice(master_dict[kk+'s'])
+        if kk != 'Model':
+            new_laptop[kk] = random.choice(master_dict[kk+'s'])
+        new_laptop['Model'] = random.choice(models[new_laptop['Brand']])
     laptop_list.append(new_laptop)
 
 user_choice = dict.fromkeys(specs)
@@ -34,5 +46,31 @@ for kk in user_choice:
     if user_choice[kk].lower() == '':
         pass
     else:
-        querry = querry + 'laptop[' + '\'' + kk + '\'' + '] == ' + '\'' + kk + '\'' + ' and '
+        querry = querry + 'laptop[' + '\'' + kk + '\'' + '] == ' + '\'' + user_choice[kk] + '\'' + ' and '
+querry = querry[0:-4:1]
 
+selected_laptops = []
+if querry == '':
+    selected_laptops = [laptop for laptop in laptop_list]
+else:
+    for laptop in laptop_list:
+        if eval(querry):
+            selected_laptops.append(laptop)
+    selected_laptops = [laptop for laptop in laptop_list if eval(querry)]
+
+
+print()
+print(len(selected_laptops), ' laptops have met your prefernces')
+characters = 0
+for kk in specs:
+    print(kk, end = '')
+    characters = len(kk)
+    print((12-characters)*' ', end = '')
+print()
+
+for laptop in selected_laptops:
+    for kk in laptop:
+        print(laptop[kk], end = '')
+        characters = len(laptop[kk])
+        print((12-characters)*' ', end = '')
+    print()
